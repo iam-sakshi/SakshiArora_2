3d384c61d73a6564b7d600465b82d14ee1b04e7f
Sakshi Arora <59931342+iam-sakshi@users.noreply.github.com>
2021-05-13 21:13:33 +0530

    Add files via upload

---
+++ SakshiArora_2/script.js
@@ -0,0 +1,10 @@
+function toggle(){
+    var blur= document.getElementById('blur');
+    blur.classList.toggle('active');
+    var msgbox= document.getElementById('msgbox');
+    msgbox.classList.toggle('active');
+    var msgimg= document.getElementById('msgimg');
+    msgimg.classList.toggle('active');
+    var imgcir= document.getElementById('imgcir');
+    imgcir.classList.toggle('active');
+}
